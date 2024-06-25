9bf17c48e935d71fc38f50d9b1cd20f8cd2b8229
Hamil <91163619+hvmil@users.noreply.github.com>
2024-06-25 12:28:23 -0400

    Time: 65 ms (31.05%), Space: 16.5 MB (99.4%) - LeetHub

---
+++ 0383-ransom-note/0383-ransom-note.py
@@ -0,0 +1,15 @@
+class Solution:
+    def canConstruct(self, ransomNote: str, magazine: str) -> bool:
+        mag_count = {}
+        for char in magazine:
+            if char in mag_count:
+                mag_count[char] += 1
+            else:
+                mag_count[char] = 1
+        for char in ransomNote:
+            if char in mag_count and mag_count[char] >= 1:
+                mag_count[char] -= 1
+            else:
+                return False
+        return True
+        
